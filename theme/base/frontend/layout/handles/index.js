7cd53a9432ffb61713c21ea8c71b034e6cf4a9cf
Skepton <xeinon@gmail.com>
2016-09-03 14:02:21 +0200

    Remade routing, routing is now a tree structure with relative paths & conditional fallback

--- theme/base/frontend/layout/handles/index.js
+++ theme/base/frontend/layout/handles/index.js
@@ -1,57 +1,79 @@
-module.exports = {
-  "get": [
-    {
-      "path": "/",
-      "handles": ['default']
-    },
-    {
-      "path": "/register",
-      "handles": ['default','userRegister']
-    },
-    {
-      "path": "/login",
-      "handles": ['default','userLogin']
-    },
-    {
-      "path": "/logout",
-      "handles": ['userLogout']
-    },
-    //Admin
-    {
-      "path": "/admin",
-      "conditional":"isAdmin",
-      "handles": ['admin','admin/postCenter']
-    },
-    {
-      "path": "/admin/composer/:hashid",
-      "conditional":"isAdmin",
-      "handles": ['admin','admin/composer']
-    }
-  ],
-  "post": [
-    {
-      "path": "/register",
-      "model": "userRegister/post"
-    },
-    {
-      "path": "/login",
-      "model": "userLogin/post"
-    },
-    //admin
-    {
-      "path":"/admin/composer/save/:hashid",
-      "model": "admin/composer/save"
-    },
-    {
-      "path":"/admin/composer/delete/:hashid",
-      "model": "admin/composer/delete"
-    }
-  ],
-  "setupRedirects": [
-    {
-      "path": "/admin/new/post",
-      "conditional":"isAdmin",
-      "model": "admin/newPost"
-    }
-  ]
-}
+module.exports = [
+  {
+    "type": "get",
+    "path": "/",
+    "handles": ['default'],
+    "childRoutes": []
+  },
+  {
+    "type": "get",
+    "path": "/register",
+    "handles": ['default','userRegister'],
+    "childRoutes": []
+  },
+  {
+    "type": "post",
+    "path": "/register",
+    "model": "userRegister/post"
+  },
+  {
+    "type": "get",
+    "path": "/login",
+    "handles": ['default','userLogin'],
+    "childRoutes": []
+  },
+  {
+    "type": "post",
+    "path": "/login",
+    "model": "userLogin/post"
+  },
+  {
+    "type": "get",
+    "path": "/logout",
+    "handles": ['userLogout'],
+    "childRoutes": []
+  },
+  //Admin
+  {
+    "type": "get",
+    "path": "/admin",
+    "conditional": "isAdmin",
+    "handles": ['admin','admin/postCenter'],
+    "childRoutes": [
+      {
+        "type": "get",
+        "path": "/settings",
+        "handles": ['admin','admin/settings'],
+        "childRoutes": [
+          {
+            "type": "get",
+            "path": "/categorizer",
+            "handles": ['admin','admin/settings/categorizer'],
+            "childRoutes": []
+          }
+        ]
+      },
+      {
+        "type": "get",
+        "path": "/composer/:hashid",
+        "handles": ['admin','admin/composer'],
+        "childRoutes": []
+      },
+      {
+        "type": "post",
+        "path": "/composer/save/:hashid",
+        "model": "admin/composer/save"
+      },
+      {
+        "type": "post",
+        "path": "/composer/delete/:hashid",
+        "model": "admin/composer/delete"
+      },
+      {
+        "type": "setupRedirects",
+        "path": "/new/post",
+        "model": "admin/setupRedirects/newPost"
+      }
+    ]
+  }
+]
